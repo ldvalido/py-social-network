8a54d8e6bf72b9be790e954cca56a4a3d3f66abd
Leandro Valido <leandro.valido@fravega.com.ar>
2023-02-11 02:42:11 -0300

    Added FastApi

--- social-network-consumer/main.py
+++ social-network-consumer/main.py
@@ -1,5 +1,8 @@
-import sys
-def main(argv):
-    pass
-if __name__ == "__main__":
-    main(sys.argv)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
