5d658bc1f96cb3402530e5f4c5e1da65ca9d0a6c
ldvalido <ldvalido@users.noreply.github.com>
2023-02-14 19:46:53 -0300

    Fanout dispatch strategy (#6)
    * Basic Folder Structure
    
    * Added FastApi
    
    * Structure for endpoints
    
    * Basic Implement for publisher
    
    * Basic Implementation for consumer with threading
    
    * Basic Docker File
    
    * Implement docker file and alpine image
    
    * Add Dockerfile for consumer
    
    ---------
    
    Co-authored-by: Leandro Valido

--- social-network-consumer/src/main.py
+++ social-network-consumer/src/main.py
@@ -4,7 +4,6 @@ from thread_listener import ThreadListener
 
 QUEUE_NAME_SOCIAL_NETWORK = "socialnetwork_posts"
 QUEUE_NAME_FOLLOWERS = "socialnetwork_follower"
-QUEUE_HOST = "localhost"
 
 def callback_post(body):
     print(f" [x] Done Post: {body}")
@@ -14,8 +13,9 @@ def callback_follower(body):
 
 def main(argv):
     print(f"PID: {os.getpid()}")
-    tFollow = ThreadListener(QUEUE_HOST, QUEUE_NAME_FOLLOWERS, callback_follower)
-    tPost = ThreadListener(QUEUE_HOST, QUEUE_NAME_SOCIAL_NETWORK, callback_post)
+    queue_host = os.getenv("QUEUE_HOST")
+    tFollow = ThreadListener(queue_host, QUEUE_NAME_FOLLOWERS, callback_follower)
+    tPost = ThreadListener(queue_host, QUEUE_NAME_SOCIAL_NETWORK, callback_post)
     tFollow.listen()
     tPost.listen()
     print("Listening")
